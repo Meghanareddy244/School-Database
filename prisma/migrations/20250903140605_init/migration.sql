fc3045133697c5dfa913e090aebf3fbf698d529c
Meghanareddy244 <meghanareddys999@gmail.com>
2025-09-03 19:46:26 +0530

    databasechanges

---
+++ prisma/migrations/20250903140605_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "public"."schools" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "contact" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "email_id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "schools_pkey" PRIMARY KEY ("id")
+);
